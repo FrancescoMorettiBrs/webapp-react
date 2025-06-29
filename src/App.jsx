1f1594a9a65709cfc6b43dde732cf367b119291e
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-06-29 18:13:40 +0200

    feat: reviews added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import GuestLayout from "./layout/GuestLayout";
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
+import SingleMovie from "./pages/SingleMovie";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
@@ -12,6 +12,7 @@ function App() {
           <Route element={<GuestLayout />}>
             <Route path="/" element={<Home />} />
             <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:id" element={<SingleMovie />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/SingleMovie.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import ReviewCard from "../components/ReviewCard";
+
+const SingleMovie = () => {
+  const { id } = useParams();
+
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/movies/${id}`).then((resp) => {
+      setMovie(resp.data.data);
+    });
+  }, []);
+  return (
+    <main>
+      {movie && movie.image && (
+        <section>
+          <img className="banner" src={movie.image} alt="" />
+        </section>
+      )}
+      {movie && (
+        <>
+          <section className="container py-4 text-center">
+            <h1>
+              {movie.title} - {movie.director}
+            </h1>
+            <div>{movie.vote_avg}</div>
+            <p>{movie.abstract}</p>
+            <section>
+              <h2>Recensioni</h2>
+              {movie.reviews.length === 0 ? (
+                <div className="alert alert-info">Momentaneamente non ci sono recensioni</div>
+              ) : (
+                <div className="row row-clos-1 g-4">
+                  {movie.reviews.map((curReview) => (
+                    <div className="col" key={curReview.id}>
+                      <ReviewCard review={curReview} />
+                    </div>
+                  ))}
+                </div>
+              )}
+            </section>
+          </section>
+        </>
+      )}
+    </main>
+  );
+};
+
+export default SingleMovie;
